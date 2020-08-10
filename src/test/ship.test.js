f6f2f3f3f5dd74bc52aaf78b6f2f467d038f532f
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-08-10 21:15:13 +0200

    Expand Ship test

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -9,3 +9,31 @@ test("Ship factory creates a ship object", () => {
   expect(ship.hit).toBeInstanceOf(Function);
   expect(ship.isSunk).toBeInstanceOf(Function);
 });
+
+describe("Ships get hit and can sink", () => {
+  let ship, shipLength = 4;
+
+  beforeEach(() => ship = ShipFactory(shipLength));
+
+  test("Ship gets hit", () => {
+    expect(() => {
+      ship.hit(0);
+      ship.hit(2);
+    }).not.toThrow();
+    
+    expect(() => {
+      ship.hit(-3);
+    }).toThrow();
+
+    expect(() => {
+      ship.hit(10);
+    }).toThrow();
+  });
+
+  test("Ship sinks", () => {
+    for(let h = 0; h < shipLength; h++) {
+      ship.hit(h);
+    }
+    expect(ship.isSunk()).toBe(true);
+  });
+});
