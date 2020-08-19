0c1e17dc043602eb778f4ca7cd498f481791b589
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-08-19 21:30:07 +0200

    Set up Webpack and React

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<p>Hello React</p>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+};
