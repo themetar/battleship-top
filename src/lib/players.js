5fde5d882dfa764cf4163d5499e78a6196f37d5a
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-09-16 18:53:48 +0200

    Make smarter AI player

--- src/lib/players.js
+++ src/lib/players.js
@@ -1,9 +1,28 @@
+import {hitTest} from "./gameboard";
+
 function AIPlayerFactory(enemyBoard) {
   let moveHandler;
 
   const takeTurn = () => {
     const available = enemyBoard.attackChart.reduce((acc, col, x) => acc.concat(col.reduce((acc, val, y) => val == "" ? acc.concat({x, y}) : acc, [])), []);
-    const choice = available[Math.floor(Math.random() * available.length)];
+    const withRating = available.map(attack => {
+      const boardSize = enemyBoard.attackChart.length;
+      const offsets = [[0, 1], [0, -1], [-1, 0], [1, 0]];
+      const neighbors = offsets.map(o => ({x: attack.x + o[0], y: attack.y + o[1]}))
+                                .filter(n => -1 < n.x && n.x < boardSize && -1 < n.y && n.y < boardSize);
+      let rating = 0;
+      const hitsNeighbors = neighbors.filter(n => enemyBoard.attackChart[n.x][n.y] == "hit" && !enemyBoard.sunkShipsLocations.some(s => hitTest(s, n))).length;
+      rating += hitsNeighbors * 100;
+
+      const missNeighbors = neighbors.filter(n => enemyBoard.attackChart[n.x][n.y] == "miss").length;
+      rating += 40 - missNeighbors * 10;
+
+      return {...attack, rating};
+    });
+
+    withRating.sort((a, b) => b.rating - a.rating);
+    const top = withRating.filter(a => a.rating == withRating[0].rating);
+    const choice = top[Math.floor(Math.random() * top.length)];
     
     if (moveHandler && typeof moveHandler == "function")
       moveHandler(choice);
